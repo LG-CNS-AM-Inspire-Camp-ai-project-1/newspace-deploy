7826f6b53b0439abdf53a435d338386e0d66734c
구동혁 <dhku@naver.com>
2025-02-21 13:05:21 +0900

    feat : Jenkins Dockerfile 업로드
    업로두

---
+++ jenkins/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+docker run -d -p 8888:8080 --name jenkins \
+  -v /home/lgcns/jenkins:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -u root \
+  --device /dev/fuse \
+  --cap-add SYS_ADMIN \
+  --security-opt apparmor:unconfined \
+  newspace/jenkins:lts
+
+if [ "$#" -eq 3 ]; then
+    MOUNT_TARGET_USER=$1
+    MOUNT_TARGET_PW=$2
+    MOUNT_TARGET_IP=$3
+
+    docker exec -it jenkins /bin/bash -c "\
+    ssh-keygen -t rsa -b 4096 -f /root/.ssh/id_rsa -N '' && \
+    sshpass -p '${MOUNT_TARGET_PW}' ssh-copy-id -i /root/.ssh/id_rsa '${MOUNT_TARGET_USER}@${MOUNT_TARGET_IP}'"
+    
+fi
