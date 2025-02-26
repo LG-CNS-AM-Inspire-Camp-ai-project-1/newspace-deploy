513c76a41394aa2dc63112ba430cb0c3264fbbc1
구동혁 <dhku@naver.com>
2025-02-26 09:31:39 +0900

    fix : github url 수정

--- jenkins/pipeline-cloud.sh
+++ jenkins/pipeline-cloud.sh
@@ -2,10 +2,10 @@
 
 currentDir=$(pwd -P);
 
-git clone https://${GIT_PASS}@github.com/LG-CNS-AM-Inspire-Camp-ai-project-1/newspace-frontend
-git clone https://${GIT_PASS}@github.com/LG-CNS-AM-Inspire-Camp-ai-project-1/newspace-deploy
-git clone https://${GIT_PASS}@github.com/LG-CNS-AM-Inspire-Camp-ai-project-1/newspace-eureka
-git clone https://${GIT_PASS}@github.com/LG-CNS-AM-Inspire-Camp-ai-project-1/newspace-gateway
+git clone https://${GIT_PASS}@github.com/newspaceProject/newspace-frontend
+git clone https://${GIT_PASS}@github.com/newspaceProject/newspace-deploy
+git clone https://${GIT_PASS}@github.com/newspaceProject/newspace-eureka
+git clone https://${GIT_PASS}@github.com/newspaceProject/newspace-gateway
 
 cp -r -f ./newspace-deploy/jenkins/deploy-cloud.sh $currentDir
 
--- jenkins/pipeline.sh
+++ jenkins/pipeline.sh
@@ -2,8 +2,8 @@
 
 currentDir=$(pwd -P);
 
-git clone https://${GIT_PASS}@github.com/LG-CNS-AM-Inspire-Camp-ai-project-1/newspace-frontend
-git clone https://${GIT_PASS}@github.com/LG-CNS-AM-Inspire-Camp-ai-project-1/newspace-deploy
+git clone https://${GIT_PASS}@github.com/newspaceProject/newspace-frontend
+git clone https://${GIT_PASS}@github.com/newspaceProject/newspace-deploy
 
 cp -r -f ./newspace-deploy/jenkins/deploy.sh $currentDir
 
